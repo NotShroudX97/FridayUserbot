a6095429f36abf670c0e3ebf80fb2d27cc0e8034
NotShroudX97 <67467393+NotShroudX97@users.noreply.github.com>
2020-12-14 01:52:13 +0530

    Update alive.py

--- fridaybot/modules/alive.py
+++ fridaybot/modules/alive.py
@@ -41,20 +41,20 @@ def get_readable_time(seconds: int) -> str:
 uptime = get_readable_time((time.time() - Lastupdate))
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = Config.ALIVE_IMAGE
-pm_caption = "➥ **FRIDAY IS:** `ONLINE`\n\n"
-pm_caption += "➥ **SYSTEMS STATS**\n"
+pm_caption = "➥ **HyperFusionX Is:** `Activated`\n\n"
+pm_caption += "➥ **System Stats**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.7.4` \n"
 pm_caption += f"➥ **Uptime** : `{uptime}` \n"
 pm_caption += "➥ **Database Status:**  `Functional`\n"
-pm_caption += "➥ **Current Branch** : `master`\n"
+pm_caption += "➥ **Current Branch** : `Alpha`\n"
 pm_caption += f"➥ **Version** : `{currentversion}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [StarkGang@Github](GitHub.com/StarkGang)\n"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/NotShroudX97/HyperFusionX/blob/master/LICENSE)\n"
+pm_caption += "➥ **Copyright** : By [NotShroudX97@Github](GitHub.com/NotShroudX97)\n"
 pm_caption += "➥ **Check Stats By Doing** `.stat`. \n\n"
-pm_caption += "[🇮🇳 Deploy FridayUserbot 🇮🇳](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "[Join Our Support Group](t.me//HyperUserBotXSupport)"
 
 
 @friday.on(friday_on_cmd(pattern=r"alive"))
